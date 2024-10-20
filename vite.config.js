d244237d81c85d4cb5456566af2342507dc4a1ea
nbnlong2712 <nhatlonghn1203@gmail.com>
2024-10-20 23:45:15 +0700

    Change branch to publish

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Tic-tac-toe---ReactJS/',
   plugins: [react()],
   build: {
     outDir: 'build'
